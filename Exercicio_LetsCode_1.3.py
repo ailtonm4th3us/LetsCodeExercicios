1be3ea96fdbe8e8ad507898d17d3dd48de7a7b1a
ailtonm4th3us <ailton.matheus@ufpe.br>
2022-02-01 09:28:04 -0300

    Oi

---
+++ Exercicio_LetsCode_1.3.py
@@ -0,0 +1,24 @@
+print('Diante do delito, aqui solicito aos suspeitos que respondam as perguntas')
+print('Os suspeitos são: Caio, Carlos e Joao. Apresente-se')
+
+pontos = 0
+
+print('Mora perto da vítima?')
+resposta_caio = input('Resposta: ')
+resposta_carlos = input('Resposta: ')
+resposta_joao = input('Resposta: ')
+
+resposta_geral = resposta_joao, resposta_caio, resposta_carlos
+
+print(resposta_geral)
+
+#if resposta_geral == 'sim'
+#    pontos = pontos + 1
+#else:
+#    print(f'1 ponto ')
+
+#print('Já trabalhou com a vítima?')
+
+#print('Telefonou para a vítima?')
+#print('Esteve no local do crime?')
+#print('Devia para a vítima?')
